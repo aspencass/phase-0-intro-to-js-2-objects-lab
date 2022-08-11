315a6189dcd1d9f47667472fdc8a474646a407b2
aspencass <caspenw1@gmail.com>
2022-08-11 07:46:32 -0600

    commit tree 648b26884b5985570fe38285d08f60688db9dc9f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+
+// const employee = {}
+// employee.name = 'bob'
+// employee.streetAddress = '123 main st'
+
+const employee = {
+    name: "cassidy",
+    streetAddress: "098 main",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+       [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee,
+    [key]: value}
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
